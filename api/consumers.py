a8543185c3494f2e0fb3c020395b6d34e7ddeb45
Anjay Goel <anjay.goel@gmail.com>
2023-01-19 02:05:35 +0530

    WIP

--- api/consumers.py
+++ api/consumers.py
@@ -1,17 +1,27 @@
 import json
-
+import time
+from enum import Enum
 from channels.generic.websocket import AsyncWebsocketConsumer, AsyncJsonWebsocketConsumer
 from channels.layers import get_channel_layer
 
 from api.utils import random_str
 
 
-class ChatConsumer(AsyncJsonWebsocketConsumer):
+class Command(Enum):
+    CHAT = "chat"
+    DISCONNECT = "disconnect_group"
+    PLAY = "play"
+    INFO = "info"
+    PAIR = "pair"
+
+
+class GameConsumer(AsyncJsonWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.group_name = None
 
     async def connect(self):
+        """
         # print(self.scope['user'])
         # print(self.scope['session'])
         # print('-------------------')
@@ -22,22 +32,34 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         # self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         # self.room_group_name = "chat_%s" % self.room_name
         # Join room group
+        """
         self.group_name = "lobby"
         await self.channel_layer.group_add("lobby", self.channel_name)
         await self.accept()
 
         lobby_channels = list(self.channel_layer.groups["lobby"].keys())
         if len(lobby_channels) > 1:
+            group_name = random_str()
+            await self.channel_layer.group_add(group_name, lobby_channels[0])
+            await self.channel_layer.group_add(group_name, lobby_channels[1])
+            await self.channel_layer.group_discard("lobby", lobby_channels[0])
+            await self.channel_layer.group_discard("lobby", lobby_channels[1])
             await self.channel_layer.group_send(
-                "lobby",
+                group_name,
                 {
                     "type": "pair",
                     "channels": [lobby_channels[0], lobby_channels[1]],
-                    "group": random_str()
+                    "group": group_name
                 }
             )
 
     async def disconnect(self, close_code):
+        await self.channel_layer.group_send(
+            self.group_name, {
+                "type": "disconnect_group",
+                "sender": self.channel_name
+            }
+        )
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive_json(self, data, **kwargs):
@@ -58,11 +80,15 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         print(f"event: {event}")
         if self.channel_name in event["channels"]:
             self.group_name = event["group"]
-            await self.channel_layer.group_add(event["group"], self.channel_name)
-            await self.channel_layer.group_discard("lobby", self.channel_name)
             await self.channel_layer.group_send(
                 self.group_name, {
                     "type": "chat_message",
                     "sender": self.channel_name,
                     "message": f"{self.channel_name} joined {event['group']}"
                 })
+
+    async def disconnect_group(self, event):
+        print(f"user disconnected from group: {event}")
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+        await self.channel_layer.group_add("lobby", self.channel_name)
+        self.group_name = "lobby"
