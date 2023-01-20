e471c4c25f825e771762279241add9cd84164932
Anjay Goel <anjay.goel@gmail.com>
2023-01-21 03:16:01 +0530

    WIP

--- api/views.py
+++ api/views.py
@@ -1,51 +1,41 @@
-from django.contrib.auth.models import User
-from rest_framework import permissions
-from rest_framework import viewsets
-from rest_framework.decorators import api_view, authentication_classes, action
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from api.authentication import GoogleJWTAuthentication
 from api.models import Player
-from api.serializers import PlayerSerializer
 from api.utils import get_user_info
-
-
-class PlayerViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = Player.objects.all()
-    serializer_class = PlayerSerializer
-    authentication_classes = [GoogleJWTAuthentication, ]
-
-    def create(self, request):
-        user_info = get_user_info(request)
-        try:
-            user = User.objects.filter(email=user_info["email"]).get()
-        except Exception as e:
-            user = User(
-                email=user_info["email"],
-                username=user_info["name"]
-            )
-            user.save()
-        player = Player(
-            user_id=user.id,
-            hall=request.data["hall"],
-            year=request.data["year"],
-            department=request.data["department"],
-            avatar=user_info["picture"]
-        )
-        player.save()
-        return Response(status=200)
-
-    @action(detail=False, methods=['get'], url_path="check")
-    def check(self, request, pk=None):
-        user_info = get_user_info(request)
-        return Response(data={"exists": self.queryset.filter(user__email=user_info['email']).count() == 1})
 
 
 @api_view(['GET'])
 @authentication_classes([GoogleJWTAuthentication])
 def get(request):
     return Response(data={"status": 200})
+
+
+@api_view(['GET'])
+@authentication_classes([])
+def check(request):
+    user_info = get_user_info(request)
+    return Response(data={"exists": Player.objects.filter(email=user_info['email']).count() == 1})
+
+
+@api_view(["POST"])
+@authentication_classes([])
+def create(request):
+    user_info = get_user_info(request)
+
+    player = Player.objects.filter(email=user_info["email"])
+    if player.count() != 0:
+        return Response(status=400, data={"message": "User already exists"})
+
+    player = Player(
+        email=user_info["email"],
+        name=user_info["name"],
+        avatar=user_info["picture"],
+        hall=request.data["hall"],
+        year=request.data["year"],
+        department=request.data["department"]
+    )
+    player.save()
+
+    return Response(status=200)
--- mtp_backend/urls.py
+++ mtp_backend/urls.py
@@ -15,15 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
 
 from api import views
 
-router = routers.DefaultRouter()
-router.register(r'player', views.PlayerViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('player/', include('api.urls')),
     path('index', views.get),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls)
