0ecb451d257638067e8d8a0165f677d09adc1419
Anjay Goel <anjay.goel@gmail.com>
2023-01-25 22:22:22 +0530

    Move to uvicorn

--- mtp_backend/settings.py
+++ mtp_backend/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['DBNAME'],
-        'HOST': os.environ['DBHOST'] + ".postgres.database.azure.com" if not DEBUG else "",
+        'HOST': os.environ['DBHOST'],
         'USER': os.environ['DBUSER'],
         'PASSWORD': os.environ['DBPASS'],
         'PORT': '5432'
